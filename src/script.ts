fc251b17a8527b93901b4a0ea2c7c9e123563a51
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-06-13 18:47:43 +0300

    ..

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,70 @@
-alert('hi')
+const run = () =>{
+    const canvas: any = document.getElementById('myCanvas');
+    const context: any = canvas.getContext('2d')!;
+
+    const inputField: any = document.getElementById("name");
+    const downloadCardButton = document.getElementById("downloadCard");
+    let positionX: any = document.getElementById("position_x");
+    let positionY: any = document.getElementById("position_y");
+    let fontSize: any = document.getElementById("font_size");
+    let fontColor: any = document.getElementById("text_color");
+    
+    const imageObj = new Image();
+    imageObj.src = "assets/images/card-image.jpg";
+
+    imageObj.onload = () => {
+        canvas.width = imageObj.width;
+        canvas.height = imageObj.height;
+        context.drawImage(imageObj, 0, 0);
+
+        positionX.max = canvas.width;
+        fontSize.max = canvas.width;
+        positionY.max = canvas.height;
+        
+        positionX.value = canvas.width / 2;
+        fontSize.value = Math.round(canvas.height / 10);
+        positionY.value = parseInt(fontSize.value) + 100;
+        redrawCanvas()
+    };
+
+    function DownloadCanvasAsImage() {
+        const downloadLink = document.createElement("a");
+        downloadLink.setAttribute("download", "jcdc-2023-adha-card.png");
+
+        canvas.toBlob((blob) => {
+            const url = URL.createObjectURL(blob);
+            downloadLink.setAttribute("href", url);
+            downloadLink.click();
+        });
+    }
+   
+    function redrawCanvas(){
+        alert('redraw')
+        const text = inputField.value;
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.drawImage(imageObj, 0, 0);
+        context.textAlign = "center";
+        context.font = `${fontSize.value}px Arial`;
+        context.fillStyle = fontColor.value;
+    
+        context.fillText(text, positionX.value, positionY.value);
+    }
+
+    document.addEventListener("DOMContentLoaded", redrawCanvas);
+    fontSize.addEventListener("input", redrawCanvas);
+    fontColor.addEventListener("input", redrawCanvas);
+    positionX.addEventListener("input", redrawCanvas);
+    positionY.addEventListener("input", redrawCanvas);
+    inputField.addEventListener("input", redrawCanvas);
+    downloadCardButton.addEventListener("click", DownloadCanvasAsImage);
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    alert('dom loaded')
+    run();
+});
+  
